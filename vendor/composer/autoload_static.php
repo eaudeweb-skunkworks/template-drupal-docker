d9a8c95b39a215a729c8d97fb46744f8a9e872e8
Cristian Romanescu <cristian@romanescu.ro>
2020-11-22 19:15:01 +0200

    Update core 8.9.9

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -544,6 +544,7 @@ class ComposerStaticInitca78efd7002b1de8846fd3516af44cea
         'ArithmeticError' => __DIR__ . '/..' . '/symfony/polyfill-php70/Resources/stubs/ArithmeticError.php',
         'AssertionError' => __DIR__ . '/..' . '/symfony/polyfill-php70/Resources/stubs/AssertionError.php',
         'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
         'DivisionByZeroError' => __DIR__ . '/..' . '/symfony/polyfill-php70/Resources/stubs/DivisionByZeroError.php',
         'Drupal' => __DIR__ . '/../..' . '/web/core/lib/Drupal.php',
         'DrupalFinder\\DrupalFinder' => __DIR__ . '/..' . '/webflo/drupal-finder/src/DrupalFinder.php',
