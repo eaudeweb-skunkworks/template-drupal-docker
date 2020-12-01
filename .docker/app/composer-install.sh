26bd5b797e5f18d68cb7fffb001935fc6f6daa94
Cristian Romanescu <cristian@romanescu.ro>
2020-12-01 23:52:19 +0200

    Switch to new image

--- .docker/app/composer-install.sh
+++ .docker/app/composer-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+cd /tmp
 EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
