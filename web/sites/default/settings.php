7fe855375cbebfc385fb5e07e07e03eabf147722
Cristian Romanescu <cristian@romanescu.ro>
2020-12-07 22:13:32 +0200

    Fix SMTP config

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -820,7 +820,7 @@ $databases['default']['default'] = [
 
 # SMTP module
 if (!empty(getenv('DRUPAL_SMTP_HOST_ON'))) {
-  $config['smtp.settings']['smtp_on'] = TRUE;
+  $config['smtp.settings']['smtp_on'] = true;
 }
 if (!empty(getenv('DRUPAL_SMTP_HOST'))) {
   $config['smtp.settings']['smtp_host'] = getenv('DRUPAL_SMTP_HOST');
